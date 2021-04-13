82a2937cf5f0436361919b637b178beb365d482e
MichaelYashchenko <78351504+MichaelYashchenko@users.noreply.github.com>
2021-04-13 15:01:00 +0300

    first commit

---
+++ 03/main.cpp
@@ -0,0 +1,43 @@
+#include "matrix.h"
+
+void Tests() {
+	Matrix m(3, 3);
+	Matrix m1(3,3);
+	Matrix m2(3,3);
+	Matrix m3(3,3);
+	for (size_t i = 0; i != 3; ++i) {
+		for (size_t j = 0; j != 3; ++j) {
+			m[i][j] = 1;
+			m1[i][j] = 1;
+			m2[i][j] = 0;
+			m3[i][j] = 5;
+		}
+	}
+	std::cout << "Starting tests:\n";
+	assert(m != m2);
+	std::cout << "Test 1 passed\n";
+	assert(m == m1);
+	std::cout << "Test 2 passed\n";
+	m *= 5;
+	assert(m == m3);
+	std::cout << "Test 3 passed\n";
+	assert((m.getRows() == 3) && (m.getCols() == 3));
+	std::cout << "Test 4 passed\n";
+}
+
+void StreamTest() {
+    Matrix m(3, 3);
+    for (size_t i = 0; i != 3; ++i) {
+	for (size_t j = 0; j != 3; ++j) {
+			m[i][j] = 1;
+		}
+	}
+    std::cout << m;
+}
+
+int main() {
+    Tests();
+    StreamTest();
+	std::cout << "All tests successfully passed\n";
+	return 0;
+}
---
+++ 03/matrix.cpp
@@ -0,0 +1,79 @@
+#include "matrix.h"
+
+Matrix::~Matrix() {
+	if (arr != nullptr) {
+		for (size_t i = 0; i != rows; ++i) {
+			delete[] arr[i];
+		}
+	}
+	delete[] arr;
+}
+
+Matrix::Proxy::Proxy(int* ii, size_t jj) {
+	i = ii;
+	j = jj;
+}
+
+int& Matrix::Proxy::operator[] (size_t jj) {
+	if (jj > j) {
+		throw std::out_of_range("");
+	}
+	return i[jj];
+}
+
+Matrix::Proxy Matrix::operator[] (size_t i) {
+	if (i > rows) {
+		throw std::out_of_range("");
+	}
+	return Matrix::Proxy(arr[i], cols);
+}
+
+Matrix::Matrix(size_t r, size_t c) {
+	rows = r;
+	cols = c;
+	arr = new int*[rows];
+	for (size_t i =0; i !=cols; ++i) {
+		arr[i] = new int[cols];
+	}
+}
+
+size_t Matrix::getRows() {
+	return rows;
+}
+
+size_t Matrix::getCols() {
+	return cols;
+}
+
+Matrix& Matrix::operator*= (int k) {
+	for (size_t i = 0; i != rows; ++i) {
+		for (size_t j = 0; j != cols; ++j) {
+			arr[i][j] *= k;
+		}
+	}
+	return *this;
+}
+
+bool Matrix::operator== (const Matrix& m) {
+	if ((rows != m.rows) || (cols != m.cols)) return false;
+	for (size_t i = 0; i != rows; ++i) {
+		for (size_t j = 0; j != cols; ++j) {
+			if (arr[i][j] != m.arr[i][j]) return false;
+		}
+	}
+	return true;
+}
+
+std::ostream& operator<<(std::ostream &out, const Matrix &matrix) {
+	for (size_t i=0; i < matrix.rows; i++) {
+		for (size_t j=0; j < matrix.cols; j++) {
+			out << matrix.arr[i][j] << " ";
+		}
+		out << '\n';
+	}
+	return out;
+}
+
+bool Matrix::operator!= (const Matrix& m) {
+	return !(*this == m);
+}
