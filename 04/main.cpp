63f2aa76f055dba40978721f5ec02de96002b297
MichaelYashchenko <78351504+MichaelYashchenko@users.noreply.github.com>
2021-04-17 19:06:23 +0300

    first commit

---
+++ 04/main.cpp
@@ -0,0 +1,30 @@
+#include "BigInt.h"
+
+void Tests() {
+	BigInt a("10999999999999999999999999");
+	BigInt b("30001023123421345423542353");
+	BigInt c = -b;
+	BigInt t(-1);
+	BigInt zero(0);
+	std::cout << "Starting tests:";
+	std:: cout << "\nshould be 0, got " << zero;
+	std::cout << "\nshould be 0, got " << b + c;
+	std::cout << "\nshould be -19001023123421345423542354, got " << a - b;
+	c = 500;
+	t = -400;
+	BigInt p(500);
+	std::cout << "\nshould be 100, got " << t + c;
+    assert(c >= t);
+    assert(t != c);
+    assert(c == p);
+    assert(t <= c);
+    assert(t < c);
+    assert(c > t);
+    std::cout << "Tests are over, check if all is correct\n";
+}
+
+
+int main() {
+    Tests();
+	return 0;
+}
