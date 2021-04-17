20e871dbe89603e84865e7b9666b5a2a152e7a72
MichaelYashchenko <78351504+MichaelYashchenko@users.noreply.github.com>
2021-04-17 19:09:34 +0300

    first commit

--- 04/main.cpp
+++ 04/main.cpp
@@ -1,19 +1,19 @@
 #include "BigInt.h"
 
 void Tests() {
-	BigInt a("10999999999999999999999999");
-	BigInt b("30001023123421345423542353");
-	BigInt c = -b;
-	BigInt t(-1);
-	BigInt zero(0);
-	std::cout << "Starting tests:";
-	std:: cout << "\nshould be 0, got " << zero;
-	std::cout << "\nshould be 0, got " << b + c;
-	std::cout << "\nshould be -19001023123421345423542354, got " << a - b;
-	c = 500;
-	t = -400;
-	BigInt p(500);
-	std::cout << "\nshould be 100, got " << t + c;
+    BigInt a("10999999999999999999999999");
+    BigInt b("30001023123421345423542353");
+    BigInt c = -b;
+    BigInt t(-1);
+    BigInt zero(0);
+    std::cout << "Starting tests:";
+    std:: cout << "\nshould be 0, got " << zero;
+    std::cout << "\nshould be 0, got " << b + c;
+    std::cout << "\nshould be -19001023123421345423542354, got " << a - b;
+    c = 500;
+    t = -400;
+    BigInt p(500);
+    std::cout << "\nshould be 100, got " << t + c;
     assert(c >= t);
     assert(t != c);
     assert(c == p);
@@ -26,5 +26,5 @@ void Tests() {
 
 int main() {
     Tests();
-	return 0;
+    return 0;
 }
