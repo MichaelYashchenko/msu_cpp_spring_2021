319380a8ffc66344ed784a473e95bc815f4af488
MichaelYashchenko <78351504+MichaelYashchenko@users.noreply.github.com>
2021-05-07 00:26:19 +0300

    first commit

---
+++ 07/allocator.hpp
@@ -0,0 +1,22 @@
+#pragma once
+#include "includes.hpp"
+
+template <class T>
+class allocator {
+	public:		
+		void destroy(T* ptr) {
+			ptr->~T();
+		}
+		void deallocate(T* ptr, size_t num) {
+			free(ptr);
+		}
+		void construct(T* ptr, const T& val) {
+			new(ptr) T(val);
+		}
+		T* allocate(size_t num) {
+			auto ptr = static_cast<T*>(malloc(num * sizeof(T)));
+			if (ptr == NULL)
+				throw std::bad_alloc();
+			return ptr;
+		}
+};
---
+++ 07/main.cpp
@@ -0,0 +1,46 @@
+#include  "allocator.hpp"
+#include "iterator.hpp"
+#include "vector.hpp"
+#include "includes.hpp"
+
+void ResizeTest() {
+    vector<int> vec(10, -1);
+	vec.resize(2, 3);
+	assert(vec[0] == -1);
+}
+
+void EmtpyVecTest() {
+    vector<int> vec(10, -1);
+    vec.resize(0);
+    assert(vec.empty());
+	int a = 5;
+	vec.push_back(a);
+	assert(vec[0] == 5);
+	vec.clear();
+	assert(vec.empty());
+}
+
+void SizeTest() {
+    vector<int> vec(10, -1);
+    vec.resize(5, 1);
+	assert(vec.size() == 5);
+}
+
+void IteratorTest() {
+    vector<int> vec(5, 1);
+    vec[2] = 2;
+	auto it = vec.begin();
+	assert(*(it + 2) == 2);
+	it = vec.end();
+	assert(*(it - 1) == 1);
+}
+
+int main() {
+	std::cout << "Starting tests\n";
+    ResizeTest();
+    EmtpyVecTest();
+    SizeTest();
+    IteratorTest();
+	std::cout << "All tests passed\n";
+	return 0;
+}
